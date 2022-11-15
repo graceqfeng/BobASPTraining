06efbbc218d0a957fe277611df23beaf18270988
graceqfeng <49483184+graceqfeng@users.noreply.github.com>
2022-11-15 16:09:30 -0500

    Initial commit

---
+++ CoursesApiSolution/CoursesApi/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesApi.Controllers;
+
+public class CoursesController : ControllerBase
+{
+
+    private readonly CourseCatalog _catalog;
+
+    public CoursesController(CourseCatalog catalog)
+    {
+        _catalog = catalog;
+    }
+
+    [HttpGet("/courses")]
+   
+    public async  Task<ActionResult> GetCoursesAsync(CancellationToken token)
+    {
+        CoursesResponseModel response = await _catalog.GetFullCatalogAsync(token);
+
+        //var response = new CoursesResponseModel { NumberOfBackendCourses = 99, NumberOfFrontendCourses = 12 };
+        return Ok(response);
+    }
+}
